0763d782930a9c9c016ada9fd141b05d7649886e
vikasvd <vickylokhande19991999@protonmail.com>
2024-12-01 16:42:51 +0530

    .

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,17 +1,19 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
+import logo from "./logo.png";
+import new_logo from "./new_logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import profile_image from "./profile_image.png";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.png";
 
-export const assets ={
+export const assets = {
     logo,
+    new_logo,
     add_icon,
     order_icon,
     profile_image,
     upload_area,
-    parcel_icon
-}
+    parcel_icon,
+};
 
-export const url = 'https://food-delivery-backend-l3j1.onrender.com'
+export const url = "https://food-delivery-backend-l3j1.onrender.com";
