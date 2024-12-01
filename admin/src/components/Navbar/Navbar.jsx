fa49868e69b84e6b98a58124567b1904ee1193b3
vikasvd <vickylokhande19991999@protonmail.com>
2024-12-01 08:30:57 +0530

    modified:   admin/src/components/Navbar/Navbar.jsx

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img className="logo" src={assets.new_logo} alt="" />
-            <img className="profile" src={assets.profile_image} alt="" />
+            <img className="logo" src={assets.new_logo} alt="logo" />
+            <img className="profile" src={assets.profile_image} alt="profile" />
         </div>
     );
 };
