97bc09a88cfd81143b0f09f00f4a4129388ed718
vikasvd <vickylokhande19991999@protonmail.com>
2024-12-01 07:30:44 +0530

    added dot cart css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-// import { useContext, useEffect, useState } from "react";
+// import { useContext, useState } from "react";
 // import "./Navbar.css";
 // import { assets } from "../../assets/frontend_assets/assets";
 // import { Link, useNavigate } from "react-router-dom";
@@ -6,28 +6,7 @@
 
 // const Navbar = ({ setShowLogin }) => {
 //     const [menu, setMenu] = useState("home");
-//     const [showNavbar, setShowNavbar] = useState(true); // State for showing/hiding navbar
 //     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
-
-//     let lastScrollTop = 0;
-
-//     useEffect(() => {
-//         const handleScroll = () => {
-//             const scrollTop =
-//                 window.pageYOffset || document.documentElement.scrollTop;
-
-//             if (scrollTop > lastScrollTop) {
-//                 setShowNavbar(false); // Scrolling down: hide navbar
-//             } else {
-//                 setShowNavbar(true); // Scrolling up: show navbar
-//             }
-
-//             lastScrollTop = scrollTop;
-//         };
-
-//         window.addEventListener("scroll", handleScroll);
-//         return () => window.removeEventListener("scroll", handleScroll);
-//     }, []);
 
 //     const navigate = useNavigate();
 
@@ -38,9 +17,9 @@
 //     };
 
 //     return (
-//         <div className="navbar" style={{ top: showNavbar ? "0" : "-70px" }}>
+//         <div className="navbar">
 //             <Link to="/">
-//                 <img src={assets.logo} alt="" className="logo" />
+//                 <img src={assets.new_logo} alt="" className="logo" />
 //             </Link>
 //             <ul className="navbar-menu">
 //                 <Link
@@ -48,47 +27,47 @@
 //                     onClick={() => setMenu("home")}
 //                     className={menu === "home" ? "active" : ""}
 //                 >
-//                     home
+//                     HOME
 //                 </Link>
 //                 <a
 //                     href="#explore-menu"
 //                     onClick={() => setMenu("menu")}
 //                     className={menu === "menu" ? "active" : ""}
 //                 >
-//                     menu
+//                     MENU
 //                 </a>
 //                 <a
 //                     href="#app-download"
 //                     onClick={() => setMenu("mobile-app")}
 //                     className={menu === "mobile-app" ? "active" : ""}
 //                 >
-//                     mobile-app
+//                     MOBILE-APP
 //                 </a>
 //                 <a
 //                     href="#footer"
 //                     onClick={() => setMenu("contact-us")}
 //                     className={menu === "contact-us" ? "active" : ""}
 //                 >
-//                     contact-us
+//                     CONTACT-US
 //                 </a>
 //             </ul>
 //             <div className="navbar-right">
 //                 <img src={assets.search_icon} alt="search icon" />
-//                 <div className="navbar-search-icon">
+//                 <div className="navbar-cart-icon">
 //                     <Link to="/cart">
-//                         <img src={assets.basket_icon} alt="basket icon" />
+//                         <img src={assets.basket_icon} alt="cart icon" />
 //                     </Link>
 //                     <div
 //                         className={getTotalCartAmount() === 0 ? "" : "dot"}
 //                     ></div>
 //                 </div>
 //                 {!token ? (
-//                     <button onClick={() => setShowLogin(true)}>sign in</button>
+//                     <button onClick={() => setShowLogin(true)}>Sign In</button>
 //                 ) : (
 //                     <div className="navbar-profile">
 //                         <img src={assets.profile_icon} alt="profile_icon" />
 //                         <ul className="nav-profile-dropdown">
-//                             <li onClick={() => navigate("/myorder")}>
+//                             <li onClick={() => navigate("/myorders")}>
 //                                 <img src={assets.bag_icon} alt="bag-icon" />
 //                                 <p>Orders</p>
 //                             </li>
@@ -109,6 +88,8 @@
 // };
 
 // export default Navbar;
+
+// ==================================================================================================
 
 import { useContext, useState } from "react";
 import "./Navbar.css";
@@ -165,7 +146,7 @@ const Navbar = ({ setShowLogin }) => {
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search icon" />
-                <div className="navbar-cart-icon">
+                <div className="navbar-search-icon">
                     <Link to="/cart">
                         <img src={assets.basket_icon} alt="cart icon" />
                     </Link>
